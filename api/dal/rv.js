6d6961526e99bc9f2c58a5927f4d2d1e8ba098f0
roney <ningrao@126.com>
2016-12-23 15:20:36 +0800

    u

--- api/dal/rv.js
+++ api/dal/rv.js
@@ -4,15 +4,21 @@ const mgs=require('mongoose')
 ,loc=mgs.model('Location');
 
 module.exports={
-	create(obj, cb){
-		loc.create(obj, function(err, rs){
+	create(locId, obj, cb){
+		loc.findOneAndUpdate({_id:locId}, {$push:{reviews:obj}}, null, function(err, rs){
 			if(err){
-				debug('loc.create error:', err);
+				debug('review.create error:', err);
 			}
 			cb(err, rs);
 		});
 	}
-	,update(){
+	,update(locId, text, cb){
+		loc.findOneAndUpdate({_id:locId}, {$set:{'reviews.$.reviewText':text}}, null, function(err, rs){
+			if(err){
+				debug('review.create error:', err);
+			}
+			cb(err, rs);
+		});
 	}
 	,remove(){
 	}
